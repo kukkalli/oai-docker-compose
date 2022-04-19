b40e4a9a3d34423f2e4a23be4cceb95ed057e52d
kukkalli <kukkalli@outlook.com>
2022-04-19 21:13:49 +0200

    MME WIP 20220419_21:13

--- 4g/mme/mme-cfg.sh
+++ 4g/mme/mme-cfg.sh
@@ -2,9 +2,10 @@
 
 INSTANCE=1
 PREFIX='/magma-mme/etc'
-MY_REALM="${REALM}"  # 'openairinterface.org'
+MY_REALM="${MME_CONF[@REALM@]}"
 
-echo "HSS IP is: " + "$HSS_IP"
+echo "HSS IP is: ${MME_CONF[@HSS_IP@]}"
+echo "MME FQDN is: ${MME_CONF[@MME_FQDN@]}"
 
 
 declare -A MME_CONF
@@ -14,15 +15,28 @@ MME_CONF[@MME_S6A_IP_ADDR@]="192.168.68.149"
 MME_CONF[@INSTANCE@]=$INSTANCE
 MME_CONF[@PREFIX@]=$PREFIX
 MME_CONF[@REALM@]=$MY_REALM
-MME_CONF[@MME_FQDN@]="mme.${MME_CONF[@REALM@]}"
-MME_CONF[@HSS_HOSTNAME@]='hss'
-MME_CONF[@HSS_FQDN@]="${MME_CONF[@HSS_HOSTNAME@]}.${MME_CONF[@REALM@]}"
-MME_CONF[@HSS_IP_ADDR@]="$HSS_IP" # "192.168.68.130"
+MME_CONF[@MME_FQDN@]="${MME_CONF[@MME_FQDN@]}"
+MME_CONF[@HSS_HOSTNAME@]="${MME_CONF[@HSS_HOSTNAME@]}"
+MME_CONF[@HSS_FQDN@]="${MME_CONF[@HSS_FQDN@]}"
+MME_CONF[@HSS_IP_ADDR@]="${MME_CONF[@HSS_IP@]}"
+MME_CONF[@MCC@]="${MME_CONF[@MCC@]}"
+MME_CONF[@MNC@]="${MME_CONF[@MNC@]}"
+MME_CONF[@MME_GID@]="${MME_CONF[@MME_GID@]}"
+MME_CONF[@MME_CODE@]="${MME_CONF[@MME_CODE@]}"
+MME_CONF[@SGWC_IP_ADDRESS@]="${MME_CONF[@SGWC_IP_ADDRESS@]}"
+
 
 cp mme_fd.conf.tmplt $PREFIX/mme_fd.conf
 
 for K in "${!MME_CONF[@]}"; do
+  echo "K is ${K}"
   egrep -lRZ "$K" $PREFIX/mme_fd.conf | xargs -0 -l sed -i -e "s|$K|${MME_CONF[$K]}|g"
+  ret=$?;[[ ret -ne 0 ]] && echo "Tried to replace $K with ${MME_CONF[$K]}"
+done
+
+for K in "${!MME_CONF[@]}"; do
+  echo "K in mme.conf is ${K}"
+  egrep -lRZ "$K" $PREFIX/mme.conf | xargs -0 -l sed -i -e "s|$K|${MME_CONF[$K]}|g"
   ret=$?;[[ ret -ne 0 ]] && echo "Tried to replace $K with ${MME_CONF[$K]}"
 done
 
