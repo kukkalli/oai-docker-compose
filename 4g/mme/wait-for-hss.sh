7b39b85d100edcc9841a02dd6fd9f4889cfca55e
kukkalli <kukkalli@outlook.com>
2022-04-20 18:52:51 +0200

    Create docker-compose for SPGW-C

--- 4g/mme/wait-for-hss.sh
+++ 4g/mme/wait-for-hss.sh
@@ -10,7 +10,8 @@ echo "Waiting for HSS to launch on 3868..."
 HSS_IP="$1"
 
 while ! nc -z "$HSS_IP" 3868; do
-  sleep 10 # wait for 1/10 of the second before check again
+  echo "MME at IP $HSS_IP not connected"
+  sleep 0.10 # wait for 1/10 of the second before check again
 done
 
 echo "HSS launched"
---
+++ 4g/spgw-c/wait-for-mme.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# wait-for-mme.sh
+
+sudo apt update
+
+sudo apt install -y netcat
+
+set -e
+echo "Waiting for MME to launch on 3870..."
+MME_IP="$1"
+
+while ! nc -z "$MME_IP" 3870; do
+  echo "MME at IP $MME_IP not connected"
+  sleep 0.10 # wait for 1/10 of the second before check again
+done
+
+echo "MME launched"
+
