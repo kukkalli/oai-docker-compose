1231cdeed520a11ea13716e01c69b4de1e9143a5
kukkalli <kukkalli@outlook.com>
2022-06-16 17:31:56 +0200

    Refactor name and position

--- 4g/spgw-u/wait-for-sgw-c.sh
+++ 4g/spgw-u/wait-for-sgw-c.sh
@@ -7,10 +7,10 @@ sudo apt install -y netcat
 
 set -e
 echo "Waiting for SPGW-C to launch on 2123..."
-MME_IP="$1"
+SPGWC_IP="$1"
 
-while ! nc -z -u "$MME_IP" 2123; do
-  echo "SPGW-C at IP $MME_IP not connected"
+while ! nc -z -u "$SPGWC_IP" 2123; do
+  echo "SPGW-C at IP $SPGWC_IP not connected"
   sleep 0.10 # wait for 1/10 of the second before check again
 done
 
